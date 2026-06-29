d4900668af4ed02b98ecfb136fef6c76f77d1b57
Sofiya Filipovich <filipovichsofiya@gmail.com>
2026-06-29 20:09:16 +0200

    Add InventoryPage POM and cart test

---
+++ pages/InventoryPage.ts
@@ -0,0 +1,13 @@
+import {Page} from '@playwright/test' 
+export class InventoryPage {
+    readonly page: Page; 
+constructor (page: Page) {
+this.page = page; 
+}
+async addToCart() {
+    await this.page.locator('[data-test="add-to-cart-sauce-labs-backpack"]').click();
+  }
+  getCartBadge() {
+    return this.page.locator('.shopping_cart_badge');
+  }
+}
--- tests/saucedemo-pom.spec.ts
+++ tests/saucedemo-pom.spec.ts
@@ -1,5 +1,7 @@
 import {test, expect} from '@playwright/test';
 import {LoginPage} from '../pages/LoginPage';
+import {InventoryPage} from '../pages/InventoryPage';
+
 test('successful login', async ({ page }) => {
   const loginPage = new LoginPage(page);
   await loginPage.goto();
@@ -12,4 +14,13 @@ test('login with wrong password', async ({ page }) => {
   await loginPage.goto();
   await loginPage.login('standard_user', 'wrong_password');
   await expect(loginPage.getErrorMessage()).toBeVisible();
+});
+
+test('Add goods', async ({ page }) => {
+  const loginPage = new LoginPage(page);
+  const inventoryPage = new InventoryPage(page);
+  await loginPage.goto();
+  await loginPage.login('standard_user', 'secret_sauce');
+  await inventoryPage.addToCart();
+  await expect(inventoryPage.getCartBadge()).toHaveText('1');
 });
